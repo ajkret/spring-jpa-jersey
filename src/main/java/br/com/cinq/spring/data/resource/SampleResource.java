ef910f91b1bb75d856eaf3b8a89f487cad5a2fd8
a.kretschmer <ajkret@hotmail.com>
2016-06-01 13:52:04 -0300

    Removed business rules from controller layer.

--- src/main/java/br/com/cinq/spring/data/resource/SampleResource.java
+++ src/main/java/br/com/cinq/spring/data/resource/SampleResource.java
@@ -1,6 +1,5 @@
 package br.com.cinq.spring.data.resource;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -16,41 +15,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.cinq.spring.data.sample.domain.CityDomain;
-import br.com.cinq.spring.data.sample.domain.CountryDomain;
 import br.com.cinq.spring.data.sample.entity.City;
-import br.com.cinq.spring.data.sample.entity.Country;
 
 /**
- * Greet Service
+ * Sample implementation of JAX-WS with Jersey.
  *
  * @author Adriano Kretschmer
  */
 @Path("/")
 public class SampleResource {
-    Logger logger = LoggerFactory.getLogger(SampleResource.class);
+    static Logger logger = LoggerFactory.getLogger(SampleResource.class);
 
     @Autowired
     CityDomain cityDomain;
 
-    @Autowired
-    CountryDomain countryDomain;
-
-    @Path("/cities")
+    @Path("cities")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response findCities(@QueryParam("country") String name) {
-        List<City> result = new LinkedList<City>();
+        List<City> result = null;
         try {
-            logger.info("Retrieving cities for {}", name);
-            
-            List<Country> countries = countryDomain.listCountries(name);
-            
-            for(Country country : countries) {
-                List<City> cities = cityDomain.listCities(country);
-                
-                result.addAll(cities);
-            }
-
+            result = cityDomain.findCitiesByCountry(name);
         } catch (Exception e) {
             logger.error("An exception occurred while retrieving cities", e);
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity("exception").build();
--- src/main/java/br/com/cinq/spring/data/sample/domain/CityDomain.java
+++ src/main/java/br/com/cinq/spring/data/sample/domain/CityDomain.java
@@ -1,15 +1,21 @@
 package br.com.cinq.spring.data.sample.domain;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.transaction.Transactional;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.cinq.spring.data.sample.entity.City;
 import br.com.cinq.spring.data.sample.entity.Country;
 import br.com.cinq.spring.data.sample.repository.CityRepository;
+import br.com.cinq.spring.data.sample.repository.CountryRepository;
 
 /**
  * "Domain" classes exist just to control the session / border for JPA with the
@@ -18,6 +24,10 @@ import br.com.cinq.spring.data.sample.repository.CityRepository;
  */
 @Component
 public class CityDomain {
+    static Logger logger = LoggerFactory.getLogger(CityDomain.class);
+
+    @Autowired
+    private CountryRepository countryDao;
 
     @Autowired
     private CityRepository dao;
@@ -29,5 +39,25 @@ public class CityDomain {
             return dao.findAll();
         } else
             return dao.findByCountry(country);
+    }
+
+    @Transactional(Transactional.TxType.SUPPORTS)
+    public List<City> findCitiesByCountry(String name) {
+        List<City> result = new LinkedList<City>();
+        logger.info("Retrieving cities for {}", name);
+
+        List<Country> countries = null;
+        if (name == null) {
+            countries = countryDao.findAll();
+        } else
+            countries = countryDao.findLikeName(name);
+
+        for (Country country : countries) {
+            List<City> cities = listCities(country);
+
+            result.addAll(cities);
+        }
+
+        return result;
     }
 }
